508b272a9328e20a6ee8fb1a50c124d9c0838760
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-03-12 12:17:50 +0100

    Code update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']= 'b81f169758b476bf1c1bbec7825b149a'
 
 posts = [
     {
@@ -26,6 +28,15 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route('/registration')
+def registration():
+    form = RegistrationForm()
+    return render_template('register.html',title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ =='__main__':
     app.run(debug=True)
