63e0c1348dd3af8d008d2dc5da0fd478f1cebeaf
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-03-13 00:05:00 +0100

    updates

--- app.py
+++ app.py
@@ -9,26 +9,28 @@ app.config['SECRET_KEY']= 'b81f169758b476bf1c1bbec7825b149a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+app.app_context().push()
+
 class User(db.Model):
-    id = db.column(db.Integer, primary_key = True)
-    username = db.column(db.string(20), unique=True, nullable=False)
-    email = db.column(db.string(120), unique=True, nullable=False)
-    image_file = db.column(db.string(20), nullable=False, default='default.jpg')
-    password = db.column(db.string(60), nullable=False)
-    post = db.relationship('Post', backref='author',  lazy='True')
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('post', backref='author',  lazy=True)
 
     def __repr__(self):
             return f"User('{self.username}'), '{self.email}', '{self.image_file}'"
 
 class post(db.Model):
-    id=db.column(db.Integer, primary_key=True)
-    title = db.column(db.string(100), nullable=False)
-    date_posted = db.column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.column(db.Text, nullable=False)
-    user_id = db.column(db.Interger, db.ForeignKey('user.id'), nullable=False)
+    id =  db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-            return f"User('{self.title}'), '{self.date_posted}')"
+            return f"Post('{self.title}'), '{self.date_posted}')"
 
 posts = [
     {
@@ -68,4 +70,6 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 if __name__ =='__main__':
-    app.run(debug=True)
+    with app.app_context():
+         db.create_all()
+         app.run(debug=True)
