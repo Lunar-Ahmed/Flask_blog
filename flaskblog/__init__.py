7e837182f1560efb50e6472bee8672fafbf3d966
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-03-14 23:59:44 +0100

    updat

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,3 +7,5 @@ app = Flask(__name__)
 app.config['SECRET_KEY']= 'b81f169758b476bf1c1bbec7825b149a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flaskblog import app, db
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 
-# app.app_context().push()
+app.app_context().push()
+db.create_all()
 
 posts = [
     {
