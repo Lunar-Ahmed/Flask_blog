1df5ada4f05d7995b9aa6dc3773d40062536b131
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-03-18 17:47:21 +0100

    Update

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'b81f169758b476bf1c1bbec7825b149a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -7,10 +7,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('post', backref='author',  lazy=True)
+    Posts = db.relationship('post', backref='author',  lazy=True)
 
     def __repr__(self):
-            return f"User('{self.username}'), '{self.email}', '{self.image_file}'"
+        return f"User('{self.username}'), '{self.email}', '{self.image_file}'"
 
 class Post(db.Model):
     id =  db.Column(db.Integer, primary_key=True)
@@ -20,4 +20,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-            return f"Posts('{self.title}'), '{self.date_posted}')"
+         return f"Post('{self.title}'), '{self.date_posted}')"
