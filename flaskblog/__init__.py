33ab8e3d53eefe5d268b88db4a8fa9d864f2a6e4
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-03-26 01:37:45 +0100

    updates

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY']= 'b81f169758b476bf1c1bbec7825b149a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoninManager(app)
+login_manager = LoginManager(app)
 
 
 from flaskblog import routes
